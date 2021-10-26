2dfcb311bab35e8e84a2d5bc1fe26c4f78e2dfa8
Joe_mac <joelin890120@gmail.com>
2021-10-26 18:41:09 +0800

    feat: dayFilterLogic utils

--- app/controller/fun.py
+++ app/controller/fun.py
@@ -24,3 +24,18 @@ def gSearch(context):
         return results['items']
     else:
         return "gSearch cannot use"
+
+def dayFilterLogic(context):
+
+    if "今天" in context:
+        dayFilter = date.today()
+    elif "昨天" in context:
+        dayFilter = date.today() - timedelta(days=1)
+    elif "本週" in context:
+        dayFilter = date.today() - timedelta(weeks=7)
+    elif "本月" in context:
+        dayFilter = date.today() - timedelta(days=30)
+    elif "今年" in context:
+        dayFilter = date.today() - timedelta(days=365)
+
+    return dayFilter
